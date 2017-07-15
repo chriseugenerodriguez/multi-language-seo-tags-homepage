ed2a24b480593afdc4d066dd0f9a2e36476ebf13
Chris Rodriguez <info@premiumwd.com>
2017-07-14 19:55:59 -0700

    server side html translations
    print out in html server side translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injector, PLATFORM_ID } from '@angular/core';
 import { environment } from '../environments/environment';
+import { isPlatformServer } from '@angular/common';
 
 // TRANSLATE
 import { TranslateService } from '@ngx-translate/core';
@@ -10,13 +11,16 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
 
-   constructor(translate: TranslateService) {
-
+  constructor(translate: TranslateService, injector: Injector, @Inject(PLATFORM_ID) platformId: Object) {
     translate.addLangs(['dt', 'en', 'es', 'fr', 'it', 'ja', 'ko', 'pt', 'ru', 'zhs', 'zht'])
     translate.setDefaultLang('en');
 
-    const lang: string = translate.getBrowserLang();
+    const lang = isPlatformServer(platformId) ?
+      injector.get('request').headers['accept-language'] :
+      translate.getBrowserLang();
 
-    translate.use(lang && lang.match(/dt,en,es,fr,it,ja,ko,pt,ru,zhs,zht/) ? lang : 'en' )
+    const match = (lang || '').match(/(dt|en|es|fr|it|ja|ko|pt|ru|zhs|zht)/);
+
+    translate.use(match ? match[0] : 'en');
   }
 }
